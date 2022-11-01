463fad0d4f7301814a9d295c6c7438091f819c4a
13793278836@163.com <cnxingtu@gmail.com>
2022-11-01 16:35:24 +0800

    新增公司信息

--- index.js
+++ index.js
@@ -6,20 +6,36 @@ const morgan = require("morgan");
 const logger = morgan("tiny");
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use(logger);
 
 
 app.get("/api/add_company", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    // const { action } = req.body;
-const a = await mysql.select("*").from("Counters").where("id", 1).queryRow()
-res.send(a)
-  }else {
-    res.send({code:0,msg:"暂无权限"});
-  }
+    if (req.headers["x-wx-source"]) {
+        const data = req.body;
+        data.openid = req.headers["x-wx-source"]
+        data['expire-date'] = (parseInt(data.long) || 1) * 86400 * 365 + new Date().getTime()
+        const duplicates = await mysql.select("count(1)").from("company").where("company-name", data["company-name"]).queryValue()
+        const must_in_arr = ["company-name", "company-area", "company-address", "company-master-name", "company-master-mobile", "company-main-brand", "company-type", "company-salesman", "company-construction-workers", "company-supervisors", "company-construction-workers", "expire-date"]
+        for (const item of must_in_arr) {
+            if (!Object.keys(data).includes(item)) res.send({code: 0, msg: "请检查是否有遗漏项"})
+        }
+
+        if (duplicates === 1) {
+            res.send({code: 0, msg: "名称重复"})
+        }
+        const inreview = await mysql.select("count(1)").from("company").where("openid", data["openid"]).where("status", 0).queryValue()
+        if (inreview === 1) {
+            res.send({code: 0, msg: "您有一个正在审核的公司"})
+        }
+        await mysql.insert("company", data)
+
+        res.send({code: 200, msg: "申请成功"})
+    } else {
+        res.send({code: 0, msg: "暂无权限"});
+    }
 
 })
 
@@ -27,9 +43,9 @@ res.send(a)
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
+    app.listen(port, () => {
+        console.log("启动成功", port);
+    });
 }
 
 bootstrap();
