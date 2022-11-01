a81e163494bc614e954f141d5e0d598b462644df
13793278836@163.com <cnxingtu@gmail.com>
2022-11-01 15:02:43 +0800

    更新获取openid2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -12,10 +11,6 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
 // 更新计数
 app.post("/api/count", async (req, res) => {
@@ -49,11 +44,14 @@ app.get("/api/test", async (req, res) => {
   });
 });
 
+
 // 小程序调用，获取微信 Open ID
 app.get("/api/wx_openid", async (req, res) => {
   if (req.headers["x-wx-source"]) {
     res.send({code:0,data:req.headers["x-wx-openid"]});
   }
+  res.send({code:0,data:req.headers});
+
 });
 
 const port = process.env.PORT || 80;
